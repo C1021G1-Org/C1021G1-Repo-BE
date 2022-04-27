08402a19ce71cd84976807e521bb00f2bcb2ced2
huynhtrong1711 <trongoliver1998@gmail.com>
2022-04-27 11:38:31 +0700

    update

--- C1021G1_Sprint_1/src/main/java/com/codegym/flight/service/impl/FlightService.java
+++ C1021G1_Sprint_1/src/main/java/com/codegym/flight/service/impl/FlightService.java
@@ -1,4 +1,13 @@
 package com.codegym.flight.service.impl;
 
-public class FlightService {
+import com.codegym.flight.repository.IFlightRepository;
+import com.codegym.flight.service.IFlightService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FlightService implements IFlightService {
+
+    @Autowired
+    private IFlightRepository repository;
 }
